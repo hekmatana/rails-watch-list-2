56117b41eb29902246109b668c12869d4ab7f97e
Anahita Hekmat <hekmatana@gmail.com>
2023-12-20 11:44:53 +0100

    controllers basic views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# require 'uri'
+# require 'net/http'
+# require 'json'
+
+# puts "cleaning up database..."
+# Movie.destroy_all
+# puts "database cleaned"
+
+# url = URI("http://tmdb.lewagon.com/movie/top_rated?language=en-US&page=1")
+
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+
+# request = Net::HTTP::Get.new(url)
+# request["accept"] = 'application/json'
+
+# response = http.request(request)
+# puts response.read_body
+
+# puts 'movies created'
+
+
+
+require 'uri'
+require 'net/http'
+require 'json'
+
+puts "cleaning up database..."
+Movie.destroy_all
+puts "database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+puts "Movies created"
